6dabbafdd36136197f174cc7f8ed67472fbea4c5
BruceLee <li200512182022@126.com>
2025-08-08 14:35:40 +0800

    Site updated

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/26/Hexo博客搭建踩坑记/","2025/06/26/Question-of-N-Queen/","2025/07/15/博客UI优化之旅/","2025/08/03/博客评论系统搭建指南/","2025/07/24/如何高效写博客/"];function toRandomPost(){
+var posts=["2025/06/26/Hexo博客搭建踩坑记/","2025/07/15/博客UI优化之旅/","2025/06/26/Question-of-N-Queen/","2025/07/24/如何高效写博客/","2025/08/03/博客评论系统搭建指南/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
